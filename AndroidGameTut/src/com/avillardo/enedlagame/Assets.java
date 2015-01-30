4dce41979b5ff428bf51357db5e38068676fd487
HungryRobot <HungryRobot@HungryRobot-PC>
2015-01-30 16:49:14 -0500

    finished Assets.java and LoadingScreen.java

---
+++ AndroidGameTut/src/com/avillardo/enedlagame/Assets.java
@@ -0,0 +1,11 @@
+package com.avillardo.enedlagame;
+
+import com.avillardo.framework.Image;
+import com.avillardo.framework.Sound;
+
+public class Assets {
+
+	public static Image menu;
+	public static Sound click;
+	
+}
---
+++ AndroidGameTut/src/com/avillardo/enedlagame/LoadingScreen.java
@@ -0,0 +1,57 @@
+package com.avillardo.enedlagame;
+
+import com.avillardo.framework.Game;
+import com.avillardo.framework.Graphics;
+import com.avillardo.framework.Screen;
+import com.avillardo.framework.Graphics.ImageFormat;
+
+public class LoadingScreen extends Screen {
+
+	public LoadingScreen(Game game) {
+		super(game);
+	}
+
+	@Override
+	public void update(float deltaTime) {
+		Graphics g = game.getGraphics();
+		Assets.menu = g.newImage("menu.jpg", ImageFormat.RGB565);
+		Assets.click = game.getAudio().createSound("explode.ogg");
+	
+	
+		game.setScreen(new MainMenuScreen(game));
+		
+	}
+
+	@Override
+	public void paint(float deltaTime) {
+		
+	}
+	
+	@Override
+    public void pause() {
+
+
+    }
+
+
+    @Override
+    public void resume() {
+
+
+    }
+
+
+    @Override
+    public void dispose() {
+
+
+    }
+
+
+    @Override
+    public void backButton() {
+
+
+    }
+
+}
