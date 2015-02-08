89e8db4cf6f4260ded2958f4d0924a1db93fdeaf
HungryRobot <HungryRobot@HungryRobot-PC>
2015-02-08 11:48:46 -0500

    continuing MainMenuScreen class

--- AndroidGameTut/src/com/avillardo/enedlagame/MainMenuScreen.java
+++ AndroidGameTut/src/com/avillardo/enedlagame/MainMenuScreen.java
@@ -1,5 +1,29 @@
 package com.avillardo.enedlagame;
 
-public class MainMenuScreen {
+import java.util.List;
 
+import com.avillardo.framework.Game;
+import com.avillardo.framework.Graphics;
+import com.avillardo.framework.Screen;
+import com.avillardo.framework.Input.TouchEvent;
+
+
+public class MainMenuScreen extends Screen {
+	public MainMenuScreen(Game game) {
+		super(game);
+	}
+	
+	@Override
+	public void update(float deltaTime) {
+		Graphics g = game.getGraphics();
+		List<TouchEvent> touchEvents = game.getInput().getTouchEvents();
+		
+		int len = touchEvents.size();
+		for (int i = 0; i < len; i++) {
+			
+			//CONTINUE HERE
+			
+		}
+	}
+	
 }
